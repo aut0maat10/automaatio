1405ab4fa3026c99f4087592b63dcacb00638cda
Robin Pahlman <robin.pahlman@gmail.com>
2018-11-19 00:01:58 -0500

    fix css bug

--- src/pages/press-release.js
+++ src/pages/press-release.js
@@ -1,8 +1,7 @@
 import React from 'react'
-// import { Link } from 'gatsby'
 import Image from '../components/image'
-
-// import Layout from '../components/layout'
+// import './press-release.css'
+import '../components/hero.css'
 
 const SecondPage = () => (
   <div>
